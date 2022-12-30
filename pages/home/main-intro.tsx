80ec2a739e708f03c802e66ae53107665212ff3c
JingCheng Wang <wjcauskew@hotmail.com>
2022-12-30 11:12:25 +1100

    Fill in intro and skill sections

--- pages/home/main-intro.tsx
+++ pages/home/main-intro.tsx
@@ -9,20 +9,73 @@ export default function MainIntro(){
         <h2 className="text-5xl p-5 bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 to-blue-800">
           Jingcheng Wang (Jin)
         </h2>
-        <h3 className=" text-2xl p-2">Software Engineer</h3>
-        <p className="text-md pt-5">
-          Hi there, I am a full stack software engineer who is currently working
-          at{" "}
-          <a
-            href="https://eresearch.unimelb.edu.au/"
-            target="_blank"
-            className="text-blue-700"
-            rel="noreferrer"
-          >
-            Melbourne eResearch Group
-          </a>{" "}
-          within the University of Melbourne.{" "}
-        </p>
+        <h3 className=" text-3xl p-2">Software Engineer</h3>
+        <div className="text-xl pt-5">
+          <p>
+            Hi there, I am a full stack software engineer who is currently
+            working at{" "}
+            <a
+              href="https://eresearch.unimelb.edu.au/"
+              target="_blank"
+              className="text-blue-700"
+              rel="noreferrer"
+            >
+              Melbourne eResearch Group (MeG)
+            </a>{" "}
+            within the University of Melbourne. At MeG, I am currently working
+            across the stack on a sophisticated workforce planning simulation
+            software called{" "}
+            <a
+              target="_blank"
+              className="text-blue-700"
+              rel="noreferrer"
+              href="https://www.dst.defence.gov.au/research-facility/next-generation-workforce-planning-capability/current-projects"
+            >
+              Athena Lite
+            </a>{" "}
+            for the Australian Defence Force.
+          </p>
+          <br />
+          <p>
+            I started working for the University in 2018. I have previously
+            worked in various different labs on{" "}
+            <span className="font-semibold">Software Development</span> and{" "}
+            <span className="font-semibold">Data Analysis</span> roles,
+            including :{" "}
+            <a
+              href="https://cis.unimelb.edu.au/hci"
+              target="_blank"
+              className="text-blue-700"
+              rel="noreferrer"
+            >
+              HCI Group
+            </a>
+            ,{" "}
+            <a
+              href="https://psychologicalsciences.unimelb.edu.au/chdh"
+              target="_blank"
+              className="text-blue-700"
+              rel="noreferrer"
+            >
+              Complex Human Data Hub
+            </a>
+            , and{" "}
+            <a
+              href="https://thud.msd.unimelb.edu.au/"
+              target="_blank"
+              className="text-blue-700"
+              rel="noreferrer"
+            >
+              THUD Lab
+            </a>
+            .
+          </p>
+          <br />
+          <p>
+            Please Refer to my Linkedin down below for my full professional
+            experience.
+          </p>
+        </div>
       </div>
       <div className="text-5xl flex justify-center">
         <SocialLink
--- pages/home/my-skills.tsx
+++ pages/home/my-skills.tsx
@@ -3,37 +3,43 @@ import reactIconImage from '../../public/React-icon.png'
 import backendIconImage from '../../public/backend-icon.png'
 import gitIconImage from '../../public/git-icon.png'
 import otherIconImage from '../../public/other-icon.png'
+import cloudIconImage from '../../public/cloud-icon.png'
+
 
 export default function MySkills(){
     return (
       <>
         <div>
-          <h3 className="text-3xl mt-10">My skills</h3>
+          <h3 className="text-3xl mt-20">My skills</h3>
           <p className="text-lg p-5">
-            These are the technologies that I am using/used.
+            These are the technologies that I am using / have used.
           </p>
         </div>
-        <div className="flex justify-center">
+        <div className="lg:flex justify-center gap-28">
           <SkillCard
             imageData={reactIconImage}
             title={"Frontend"}
-            items={["React", "TypeScript", "Redux"]}
+            items={["React (Redux, Next.js)", "TypeScript", "Tailwind","Cypress"]}
           />
           <SkillCard
             imageData={backendIconImage}
             title={"Backend"}
-            items={[]}
+            items={["Java(Spring,JUnit)","Node(express)","Python(Django/Flask)","PostgresSQL","MongoDB"]}
+          />
+          <SkillCard
+            imageData={cloudIconImage}
+            title={"Cloud And DevOps"}
+            items={["AWS","GCP","Auzre DevOps","Docker"]}
           />
           <SkillCard
             imageData={gitIconImage}
             title={"Productivity"}
-            items={[]}
+            items={["Git","Jira","VS Code","IntelliJ / PyCharm"]}
           />
-
           <SkillCard
             imageData={otherIconImage}
-            title={"Other"}
-            items={[]}
+            title={"Others"}
+            items={["Unity3D","OpenCV","GAN Machine Learning"]}
           />
         </div>
       </>
