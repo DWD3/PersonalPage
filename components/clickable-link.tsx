68b65c050a4754d7f356f43d66225804facd86c4
JingCheng Wang <wjcauskew@hotmail.com>
2023-01-03 15:04:27 +1100

    Add about website page

---
+++ components/clickable-link.tsx
@@ -0,0 +1,19 @@
+import { link } from "fs";
+
+type Props = {
+    link : string;
+    text : string;
+}
+
+export default function ClickableLink(props : Props){
+    return (
+      <a
+        target="_blank"
+        className="text-blue-700"
+        rel="noreferrer"
+        href={props.link}
+      >
+        {props.text}
+      </a>
+    );
+}
--- pages/home/nav-bar.tsx
+++ pages/home/nav-bar.tsx
@@ -1,16 +1,24 @@
 import { GrDocumentText } from "react-icons/gr";
 
 export default function NavBar(){
-    return <>
-              <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className=' text-2xl'>&lt;/&gt;</h1>
-            <ul className='flex items-center'>
-              <li>
-                <GrDocumentText className=''></GrDocumentText></li>
-              <li>
-                <a href='#' className='bg-gradient-to-r from-cyan-500 to-blue-500 mx-5 px-2 lg:px-4 py-2 rounded-md text-white'>Resume(TODO)</a>
-              </li>
-            </ul>
-          </nav>
+  return (
+    <>
+      <nav className="py-10 mb-12 flex justify-between">
+        <h1 className=" text-2xl">&lt;/&gt;</h1>
+        <ul className="flex items-center">
+          <li>
+            <GrDocumentText className=""></GrDocumentText>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-cyan-500 to-blue-500 mx-5 px-2 lg:px-4 py-2 rounded-md text-white"
+            >
+              Resume(TODO)
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
+  );
 }
