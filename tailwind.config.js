9753043b80aae20a1e8190e9c9f8660e08f679e2
JingCheng Wang <wjcauskew@hotmail.com>
2023-01-21 17:27:27 +1100

    Fix tailwind config after project re-structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./views/**/*.{js,ts,jsx,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
